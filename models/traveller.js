913cc89c13d5c054766bf3e3a4426b5bf568a297
tannyque <tannyque1992@gmail.com>
2018-08-22 19:22:23 +0100

    Passes calculated total distance travelled test

--- models/traveller.js
+++ models/traveller.js
@@ -33,7 +33,9 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
